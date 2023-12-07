ba7492d910e1496cd2330c0a8806f23abd757482
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2023-12-07 20:44:35 -0300

    se diseño base de datos blog

---
+++ aprendiendo-sql/03-diseño/blog.sql
@@ -0,0 +1,27 @@
+CREATE TABLE usuarios(
+id          int(255) auto_increment not null,
+nombre      varchar(100) not null,
+apellidos   varchar(100) not null,
+email       varchar(255) not null,
+password    varchar(255) not null,
+fecha       date not null,
+CONSTRAINT pk_usuarios PRIMARY KEY(id)
+);
+
+CREATE TABLE categorias(
+id      auto_increment not null,
+nombre  varchar(255) not null,
+CONSTRAINT pk_categorias PRIMARY KEY(id)
+);
+
+CREATE TABLE entradas(
+id              int(255) auto_increment not null,
+usu_id          int(255) not null,
+cat_id          int(255) not null,
+titulo          varchar(255) not null,
+descripcion     MEDIUMTEXT,
+fecha           date not null,
+CONSTRAINT pk_entradas PRIMARY KEY(id),
+CONSTRAINT fk_entradas_usuario FOREIGN KEY(usu_id) REFERENCES ususarios(id),
+CONSTRAINT fk_entradas_categoria FOREIGN KEY(cat_id) REFERENCES categorias(id) 
+);

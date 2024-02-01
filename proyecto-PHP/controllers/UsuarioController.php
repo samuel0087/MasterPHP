6764ce594c35c70725f98f8ad9998306bc08b0f1
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2024-02-01 01:17:28 -0300

    agregando registro de usuarios

--- proyecto-PHP/controllers/UsuarioController.php
+++ proyecto-PHP/controllers/UsuarioController.php
@@ -4,4 +4,12 @@ class UsuarioController{
     public function index(){
         echo "INDEX DE USUARIOS";
     }
+    
+    public function registro(){
+        require_once 'views/usuario/registro.php';
+    }
+    
+    public function save(){
+        var_dump($_POST);
+    }
 }
---
+++ proyecto-PHP/views/usuario/registro.php
@@ -0,0 +1,23 @@
+<div class="login">
+    <div class="form-container">
+      <h2 class="title">Registro de usuario</h2>
+
+      <form action="index.php?controller=Usuario&action=save" method="POST" class="form form-registro">
+          <label for="name" class="label">Nombre</label>
+          <input type="text" name="name" placeholder="Teff" class="input input-usu">
+          
+          <label for="surname" class="label">Apellidos</label>
+          <input type="text" name="surname" placeholder="Apellidos" class="input input-pass">
+          
+          
+
+          <label for="email" class="label">Email</label>
+          <input type="text" name="email" placeholder="platzi@example.com" class="input input-email">
+
+          <label for="password" class="label">Password</label>
+          <input type="password" name="password" placeholder="*********" class="input input-password">
+
+        <input type="submit" value="Create" class="primary-button login-button">
+      </form>
+    </div>
+  </div>
